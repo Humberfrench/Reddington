3ad02176e5de0d5b6af4f4b0421c6843672ca0e2
Humberto Gonçalves de Almeida <humberto.almeida@credpay.com.br>
2019-05-29 17:23:13 -0300

    impplements

---
+++ v2/Red.Web/App_Start/SimpleInjectorInitializer.cs
@@ -0,0 +1,36 @@
+using Red.Ioc;
+
+[assembly: WebActivator.PostApplicationStartMethod(typeof(Red.Web.App_Start.SimpleInjectorInitializer), "Initialize")]
+
+namespace Red.Web.App_Start
+{
+    using System.Reflection;
+    using System.Web.Mvc;
+
+    using SimpleInjector;
+    using SimpleInjector.Integration.Web;
+    using SimpleInjector.Integration.Web.Mvc;
+    
+    public static class SimpleInjectorInitializer
+    {
+        /// <summary>Initialize the container and register it as MVC3 Dependency Resolver.</summary>
+        public static void Initialize()
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+            
+            InitializeContainer(container);
+
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            
+            container.Verify();
+            
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+     
+        private static void InitializeContainer(Container container)
+        {
+            BootStrapper.Register(container);
+        }
+    }
+}
