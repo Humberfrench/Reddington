ba30218f0d228de105409ba0330f689fec78cccb
Humberto Gonçalves de Almeida <humberto.almeida@credpay.com.br>
2019-06-13 10:59:27 -0300

    update

---
+++ v2/Red.Web/Controllers/EvangelizadorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Red.Web.Controllers
+{
+    public class EvangelizadorController : Controller
+    {
+        // GET: Evangelizador
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
