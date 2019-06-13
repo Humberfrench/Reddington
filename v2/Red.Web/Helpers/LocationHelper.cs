5e4bf54c77ebab6773c2975514c3b07718a95664
Humberto Gonçalves de Almeida <humberto.almeida@credpay.com.br>
2019-06-13 16:31:54 -0300

    ajustes

---
+++ v2/Red.Web/Helpers/LocationHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Red.Web.Helpers
+{
+    public static class LocationHelper
+    {
+        public static bool IsCurrentControllerAndAction(string controllerName, string actionName, ViewContext viewContext)
+        {
+            bool result = false;
+            string normalizedControllerName = controllerName.EndsWith("Controller") ? controllerName : String.Format("{0}Controller", controllerName);
+
+            if (viewContext == null) return false;
+            if (String.IsNullOrEmpty(actionName)) return false;
+
+            if (viewContext.Controller.GetType().Name.Equals(normalizedControllerName, StringComparison.InvariantCultureIgnoreCase) &&
+                viewContext.Controller.ValueProvider.GetValue("action").AttemptedValue.Equals(actionName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+        public static bool IsCurrentController(string controllerName, ViewContext viewContext)
+        {
+            bool result = false;
+            string normalizedControllerName = controllerName.EndsWith("Controller") ? controllerName : String.Format("{0}Controller", controllerName);
+
+            if (viewContext == null) return false;
+
+            if (viewContext.Controller.GetType().Name.Equals(normalizedControllerName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
